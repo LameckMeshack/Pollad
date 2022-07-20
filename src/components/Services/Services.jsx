3aab2d33fc5d4a2bebce7b6238e17b2836b831d1
RhinolamerSonwar <rhinolamer@gmail.com>
2022-07-20 03:05:50 +0300

    second

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,31 +1,41 @@
-import React from "react";
+import "./services.css";
 
 const Services = () => {
   return (
-    <div>
+    <div className="services-container">
       <div className="service">
+        <img src="./Images/services/IT.png" alt="" height="90px" width="90px" />
         <h2>IT Management</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos beatae
           iste incidunt.
         </p>
       </div>
+
       <div className="service">
-        <h2>IT Management</h2>
+        <img
+          src="./Images/services/cloud.png"
+          alt=""
+          height="90px"
+          width="90px"
+        />
+        <h2>Cloud Services</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos beatae
           iste incidunt.
         </p>
       </div>
       <div className="service">
-        <h2>IT Management</h2>
+        <img src="./Images/services/ds.png" alt="" height="90px" width="90px" />
+        <h2>Data Security</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos beatae
           iste incidunt.
         </p>
       </div>
       <div className="service">
-        <h2>IT Management</h2>
+        <img src="./Images/services/ml.png" alt="" height="90px" width="90px" />
+        <h2>Machine Learning</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos beatae
           iste incidunt.
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,29 +7,29 @@ const Home = () => {
   return (
     <>
       <div className="bg-image">
-        <div className="home-container">
-          <div className="arrow left">
-            <AiOutlineLeft />
-          </div>
-          <div className="home-content">
-            <span className="first">We increase your</span>
-            <span className="second"> Business Success</span>
-            <span className="third">
-              Using Modern Technology and IT Services
-            </span>
-            <div className="clicks">
-              <button className="consult">Free Consultation</button>
+        <div className="section">
+          <div className="home-container">
+            <div className="arrow left">
+              <AiOutlineLeft />
+            </div>
+            <div className="home-content">
+              <span className="first">We increase your</span>
+              <span className="second"> Business Success</span>
+              <span className="third">
+                Using Modern Technology and IT Services
+              </span>
+              <div className="clicks">
+                <button className="consult">Free Consultation</button>
 
-              <IoMdPlay className="play" />
+                <IoMdPlay className="play" />
+              </div>
+            </div>
+            <div className="arrow right">
+              <AiOutlineRight />
             </div>
           </div>
-          <div className="arrow right">
-            <AiOutlineRight />
-          </div>
+          <Services />
         </div>
-      </div>
-      <div className="services-container">
-        <Services />
       </div>
     </>
   );
