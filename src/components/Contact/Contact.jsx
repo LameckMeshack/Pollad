e8225682f4dea295f3440d2be9f4f6f315098a1f
RhinolamerSonwar <rhinolamer@gmail.com>
2022-07-21 02:33:30 +0300

    finished about

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,24 @@
 import "./contact.css";
+import { MdCall } from "react-icons/md";
 
 const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-container">
-        <img src="./Images/contact/person.webp" alt="" />
-        <div className="contact-contents"></div>
+        <img src="./Images/contact/person.webp" alt="" width="40%" />
+        <div className="contact-contents">
+          <div className="call">
+            <MdCall />
+          </div>
+          <h3>CALL US 24/7</h3>
+          <h1>(+123) 456-9989</h1>
+          <h4>
+            Have any idea or project for in your mind call us or schedule a
+            <br />
+            appointment. Our representative will reply you shortly.
+          </h4>
+          <button className="btn-contact">Let's Talk</button>
+        </div>
       </div>
     </div>
   );
