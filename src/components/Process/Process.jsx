f52cf3ba2cbe7b7f33f423b9bd158958cbc443d7
RhinolamerSonwar <rhinolamer@gmail.com>
2022-07-21 18:49:28 +0300

    process done

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import "./process.css";
 
 const Process = () => {
-  return <div>Process</div>;
+  return (
+    <div className="process">
+      <div className="process-box">
+        <p>WORKING PROCESS</p>
+        <h3>Our Working Process - How We Work For Our Customers</h3>
+        <button>Contact Us</button>
+      </div>
+      <div className="process-contents">
+        <div className="process-content">
+          <h2>1.</h2>
+          <h3>Discovery</h3>
+          <p>
+            Quisque placerat vitae lacus ut scelerisque. Fusce luctus odio ac
+            nibh luctus, in porttitor theo lacus egestas
+          </p>
+        </div>
+        <div className="process-content">
+          <h2>2.</h2>
+          <h3>Planning</h3>
+          <p>
+            Quisque placerat vitae lacus ut scelerisque. Fusce luctus odio ac
+            nibh luctus, in porttitor theo lacus egestas
+          </p>
+        </div>
+        <div className="process-content">
+          <h2>3.</h2>
+          <h3>Execute</h3>
+          <p>
+            Quisque placerat vitae lacus ut scelerisque. Fusce luctus odio ac
+            nibh luctus, in porttitor theo lacus egestas
+          </p>
+        </div>
+        <div className="process-content">
+          <h2>4.</h2>
+          <h3>Deliver</h3>
+          <p>
+            Quisque placerat vitae lacus ut scelerisque. Fusce luctus odio ac
+            nibh luctus, in porttitor theo lacus egestas
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Process;
